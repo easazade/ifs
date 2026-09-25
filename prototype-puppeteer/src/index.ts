11806515cb37dc2d86ab93fba8f95ee3dae7de75
Ali <easazade@gmail.com>
2026-09-25 10:31:03 +0330

    fix(prototype-puppeteer): Fix package type not being module

--- prototype-puppeteer/src/index.ts
+++ prototype-puppeteer/src/index.ts
@@ -1,5 +1,12 @@
-function main() {
-  console.log("puppeteer is alive!");
+import { listMembers, setApiBaseUrl } from "prototype-client";
+
+async function main() {
+  console.log("STARTING!!!!!");
+
+  // Node fetch needs an absolute URL because there is no Vite development proxy.
+  setApiBaseUrl("http://localhost:3000");
+  const response = await listMembers();
+  console.log(response);
 }
 
 main();
